29f481cb0ec9db3b803f6eeebe00d5b94a55af55
Tommy 汤米 <48564227+tommyvio@users.noreply.github.com>
2025-02-02 00:53:55 -0500

    Delete src/crystalcode directory

--- src/crystalcode/init.py
+++
@@ -1,5 +0,0 @@
-from crystalcode.core.generator import CodeGenerator
-from crystalcode.core.analyzer import CodeAnalyzer
-from crystalcode.core.optimizer import CodeOptimizer
-
-__version__ = "0.2.0"
